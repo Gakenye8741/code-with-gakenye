88315836e503a659f7c127269e39e8af1f198704
Gakenye8741 <brayogakenye04@gmail.com>
2025-06-14 09:55:07 +0300

    Changed our services section

--- js files/typed.js
+++ js files/typed.js
@@ -4,4 +4,9 @@ var typed  = new Typed('#carrers',{strings:['A Frontend Web Developer','A Backen
     loop: true
     });
 
-    
+var typed = new Typed("#webDevelopment",{
+    strings: ["FullStack Web Development","FullStack Web Development"],
+    typeSpeed: 200,
+    backSpeed: 100,
+    loop: true
+});
